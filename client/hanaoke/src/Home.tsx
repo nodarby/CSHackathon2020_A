292104952f1c085970373cf2434455681a4bf686
noda tomohrio <nodatomohrio@nodanoMacBook-Pro.local>
2021-01-02 11:00:59 +0900

    メトロノーム音追加

--- client/hanaoke/src/Home.tsx
+++ client/hanaoke/src/Home.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import Button from '@material-ui/core/Button';
 import Grid from "@material-ui/core/Grid"
 import TextField from '@material-ui/core/TextField';
+import ReactHowler from 'react-howler'
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 
 // JavaScript の場合は makeStyles(theme => styleObject)で良い
@@ -24,17 +25,30 @@ type Props = {
 export function Home({title}: Props) {
     const classes = useStyles();
     const [bpm, setBpm] = useState(70);
+    const [soundStatus, setSoundStatus] = useState(false);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setBpm(Number(event.target.value))
     }
     const startRecord = () => {
-        setInterval(()=>console.log("Button clicked"),2000)
+        setInterval(
+            ()=> {
+                setSoundStatus(true)
+            }, 60000/bpm)
     }
     return (
         <Grid container alignItems="center" justify="center">
             <Grid item xs={8}>
                 <div className={classes.root}>
                     <h1>{title}</h1>
+                    <ReactHowler
+                        src="bpm_sound.mp3"
+                        playing={soundStatus}
+                        loop= {true}
+                        onEnd={() => {
+                            // 再生完了時のendイベント
+                            setSoundStatus(false)
+                        }}
+                    />
                     <TextField
                         id="standard-basic"
                         label="BPM"
