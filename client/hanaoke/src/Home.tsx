1b4f5c9a540ea542bb097078ecafe188957b3c4b
noda tomohrio <nodatomohrio@nodanoMacBook-Pro.local>
2021-01-02 16:15:27 +0900

    録音中の画面遷移

--- client/hanaoke/src/Home.tsx
+++ client/hanaoke/src/Home.tsx
@@ -25,47 +25,78 @@ type Props = {
 export function Home({title}: Props) {
     const classes = useStyles();
     const [bpm, setBpm] = useState(70);
+    const [recordStatus, setRecordStatus] = useState(false)
     const [soundStatus, setSoundStatus] = useState(false);
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setBpm(Number(event.target.value))
     }
     const startRecord = () => {
+        setRecordStatus(true)
+        //　録音中の画面に遷移
         setInterval(
             ()=> {
                 setSoundStatus(true)
             }, 60000/bpm)
     }
+
+    const stopRecord = () => {
+        setSoundStatus(false)
+        //　音声データを送信
+
+
+        //　返してもらった後の表示処理
+
+        console.log("終了")
+    }
+
+    const waiting = (
+        <div className={classes.root}>
+            <TextField
+                id="standard-basic"
+                label="BPM"
+                value={bpm}
+                type="number"
+                onChange={handleChange}
+                helperText="BPMを選択してください（70~200を推奨）"
+            />
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => startRecord()}
+            >録音開始</Button>
+        </div>
+    )
+
+    const recording = (
+        <div className={classes.root}>
+            <h2>録音中</h2>
+            <Button
+                variant="contained"
+                color="secondary"
+                onClick={() => stopRecord()}
+            >録音終了</Button>
+        </div>
+    )
+
     return (
         <Grid container alignItems="center" justify="center">
             <Grid item xs={8}>
-                <div className={classes.root}>
-                    <h1>{title}</h1>
-                    <ReactHowler
-                        src="bpm_sound.mp3"
-                        playing={soundStatus}
-                        loop= {true}
-                        onEnd={() => {
-                            // 再生完了時のendイベント
-                            setSoundStatus(false)
-                        }}
-                    />
-                    <TextField
-                        id="standard-basic"
-                        label="BPM"
-                        value={bpm}
-                        type="number"
-                        onChange={handleChange}
-                        helperText="BPMを選択してください（70~200を推奨）"
-                    />
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        onClick={() => startRecord()}
-                    >録音開始</Button>
-                </div>
+                <h1>{title}</h1>
+                <ReactHowler
+                    src="bpm_sound.mp3"
+                    playing={soundStatus}
+                    loop= {true}
+                    onEnd={() => {
+                        // 再生完了時のendイベント
+                        setSoundStatus(false)
+                    }}
+                />
+                {recordStatus ? recording : waiting}
             </Grid>
         </Grid>
     )
 }
+
+
 
 export default Home
